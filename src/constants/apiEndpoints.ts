c8d14719c03ce0d9ad63a0118b4f9440df5e886f
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-04-18 20:22:31 +0300

    FAIR-59 path alias

---
+++ src/constants/apiEndpoints.ts
@@ -0,0 +1,12 @@
+const apiEndpoints = {
+    auth: "api/v1/auth",
+	brand: "api/v1/brand",
+    item: "api/v1/item",
+    brandPost: "api/v1/brandpost",
+    topic: "api/v1/topic",
+    user: "api/v1/user",
+    userRole: "api/v1/userrole",
+    report: "api/v1/report",
+};
+
+export default apiEndpoints;
