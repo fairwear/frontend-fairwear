d59b36fe4fd5afa04c11f4eb76eed179f9938e43
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-04-18 14:00:19 +0300

    FAIR-59 ran linr

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -16,211 +16,216 @@ import SignUpDialog from "@components/login/SignUpDialog";
 import "../Components.css";
 
 const Search = styled("div")(({ theme }) => ({
-	position: "relative",
-	borderRadius: theme.shape.borderRadius,
-	backgroundColor: alpha(theme.palette.common.white, 0.15),
-	"&:hover": {
-		backgroundColor: alpha(theme.palette.common.white, 0.25),
-	},
-	marginRight: theme.spacing(2),
-	marginLeft: 0,
-	width: "70%",
-	[theme.breakpoints.up("sm")]: {
-		marginLeft: theme.spacing(3),
-		// width: 'auto',
-	},
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "70%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    // width: 'auto',
+  },
 }));
 
 const SearchIconWrapper = styled("div")(({ theme }) => ({
-	padding: theme.spacing(0, 2),
-	height: "100%",
-	position: "absolute",
-	pointerEvents: "none",
-	display: "flex",
-	alignItems: "center",
-	justifyContent: "center",
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
 }));
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
-	color: "inherit",
-	border: "1px solid black",
-	borderRadius: "10px",
-	width: "90%",
-	"& .MuiInputBase-input": {
-		padding: theme.spacing(1, 1, 1, 0),
-		// vertical padding + font size from searchIcon
-		paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-		transition: theme.transitions.create("width"),
-		// width: '100%',
-		[theme.breakpoints.up("md")]: {},
-	},
+  color: "inherit",
+  border: "1px solid black",
+  borderRadius: "10px",
+  width: "90%",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    // width: '100%',
+    [theme.breakpoints.up("md")]: {},
+  },
 }));
 
 export default function PrimarySearchAppBar() {
-	const [loginDialog, setLoginDialog] = React.useState(false);
-	const [signUpDialog, setSignUpDialog] = React.useState(false);
-	const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-	const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
-		React.useState<null | HTMLElement>(null);
+  const [loginDialog, setLoginDialog] = React.useState(false);
+  const [signUpDialog, setSignUpDialog] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] =
+    React.useState<null | HTMLElement>(null);
 
-	const isMenuOpen = Boolean(anchorEl);
-	const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+  const isMenuOpen = Boolean(anchorEl);
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-	const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-		setAnchorEl(event.currentTarget);
-	};
+  const handleProfileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-	const handleMobileMenuClose = () => {
-		setMobileMoreAnchorEl(null);
-	};
+  const handleMobileMenuClose = () => {
+    setMobileMoreAnchorEl(null);
+  };
 
-	const handleLoginDialog = () => {
-		setLoginDialog(true);
-	};
+  const handleLoginDialog = () => {
+    setLoginDialog(true);
+  };
 
-	const handleLoginDialogClose = () => {
-		setLoginDialog(false);
-		console.log("Login Dialog Closed");
-	};
+  const handleLoginDialogClose = () => {
+    setLoginDialog(false);
+    console.log("Login Dialog Closed");
+  };
 
-	const handleSignUpDialog = () => {
-		setSignUpDialog(true);
-	};
-	const handleSignUpDialogClose = () => {
-		setSignUpDialog(false);
-		console.log("Sign Up Dialog Closed");
-	};
+  const handleSignUpDialog = () => {
+    setSignUpDialog(true);
+  };
+  const handleSignUpDialogClose = () => {
+    setSignUpDialog(false);
+    console.log("Sign Up Dialog Closed");
+  };
 
-	const handleMenuClose = () => {
-		setAnchorEl(null);
-		handleMobileMenuClose();
-	};
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+    handleMobileMenuClose();
+  };
 
-	const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
-		setMobileMoreAnchorEl(event.currentTarget);
-	};
+  const handleMobileMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMobileMoreAnchorEl(event.currentTarget);
+  };
 
-	const menuId = "primary-search-account-menu";
-	const renderMenu = (
-		<Menu
-			anchorEl={anchorEl}
-			anchorOrigin={{
-				vertical: "top",
-				horizontal: "right",
-			}}
-			id={menuId}
-			keepMounted
-			transformOrigin={{
-				vertical: "top",
-				horizontal: "right",
-			}}
-			open={isMenuOpen}
-			onClose={handleMenuClose}>
-			<MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-			<MenuItem onClick={handleMenuClose}>My account</MenuItem>
-		</Menu>
-	);
+  const menuId = "primary-search-account-menu";
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+    </Menu>
+  );
 
-	const mobileMenuId = "primary-search-account-menu-mobile";
-	const renderMobileMenu = (
-		<Menu
-			anchorEl={mobileMoreAnchorEl}
-			anchorOrigin={{
-				vertical: "top",
-				horizontal: "right",
-			}}
-			id={mobileMenuId}
-			keepMounted
-			transformOrigin={{
-				vertical: "top",
-				horizontal: "right",
-			}}
-			open={isMobileMenuOpen}
-			onClose={handleMobileMenuClose}>
-			{/* <MenuItem onClick={handleProfileMenuOpen}> */}
-			<Button onClick={handleLoginDialog}>
-				<h2 className='header-text'>Log in</h2>
-			</Button>
-			{/* </MenuItem> */}
-			{/* <MenuItem onClick={handleProfileMenuOpen}> */}
-			<Button onClick={handleSignUpDialog}>
-				<h2 className='header-text'>Sign Up</h2>
-			</Button>
-			{/* </MenuItem> */}
-		</Menu>
-	);
+  const mobileMenuId = "primary-search-account-menu-mobile";
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right",
+      }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      {/* <MenuItem onClick={handleProfileMenuOpen}> */}
+      <Button onClick={handleLoginDialog}>
+        <h2 className="header-text">Log in</h2>
+      </Button>
+      {/* </MenuItem> */}
+      {/* <MenuItem onClick={handleProfileMenuOpen}> */}
+      <Button onClick={handleSignUpDialog}>
+        <h2 className="header-text">Sign Up</h2>
+      </Button>
+      {/* </MenuItem> */}
+    </Menu>
+  );
 
-	return (
-		<Box>
-			<AppBar sx={{ boxShadow: "none" }}>
-				<Toolbar sx={{ backgroundColor: "#e9e9e9" }}>
-					<IconButton
-						size='large'
-						edge='start'
-						color='inherit'
-						aria-label='open drawer'
-						sx={{
-							color: "#222222",
-							mr: 2,
-						}}>
-						<MenuIcon />
-					</IconButton>
+  return (
+    <Box>
+      <AppBar sx={{ boxShadow: "none" }}>
+        <Toolbar sx={{ backgroundColor: "#e9e9e9" }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{
+              color: "#222222",
+              mr: 2,
+            }}
+          >
+            <MenuIcon />
+          </IconButton>
 
-					<Search>
-						<SearchIconWrapper>
-							<SearchIcon sx={{ color: "#222222" }} />
-						</SearchIconWrapper>
-						<StyledInputBase
-							sx={{
-								color: "#222222",
-								width: "100%",
-								fontFamily: "Inter",
-							}}
-							placeholder='Search…'
-							inputProps={{ "aria-label": "search" }}
-						/>
-					</Search>
-					<Box sx={{ flexGrow: 1 }} />
-					<Box
-						sx={{
-							display: { xs: "none", md: "flex" },
-							flexDirection: "row",
-							justifyContent: "space-evenly",
-							width: "20%",
-							marginRight: "16px",
-						}}>
-						<Button onClick={handleLoginDialog}>
-							<h2 className='header-text'>Login</h2>
-						</Button>
-						<LoginDialog
-							toSignUp={handleSignUpDialog}
-							open={loginDialog}
-							handleClose={handleLoginDialogClose}
-						/>
-						<Button onClick={handleSignUpDialog}>
-							<h2 className='header-text'>Sign Up</h2>
-						</Button>
-						<SignUpDialog
-							toLogin={handleLoginDialog}
-							open={signUpDialog}
-							handleClose={handleSignUpDialogClose}
-						/>
-					</Box>
-					<Box sx={{ display: { xs: "flex", md: "none" } }}>
-						<IconButton
-							size='large'
-							aria-label='show more'
-							aria-controls={mobileMenuId}
-							aria-haspopup='true'
-							onClick={handleMobileMenuOpen}
-							sx={{ color: "#222222" }}>
-							<MoreIcon />
-						</IconButton>
-					</Box>
-				</Toolbar>
-			</AppBar>
-			{renderMobileMenu}
-			{renderMenu}
-		</Box>
-	);
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon sx={{ color: "#222222" }} />
+            </SearchIconWrapper>
+            <StyledInputBase
+              sx={{
+                color: "#222222",
+                width: "100%",
+                fontFamily: "Inter",
+              }}
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
+          <Box sx={{ flexGrow: 1 }} />
+          <Box
+            sx={{
+              display: { xs: "none", md: "flex" },
+              flexDirection: "row",
+              justifyContent: "space-evenly",
+              width: "20%",
+              marginRight: "16px",
+            }}
+          >
+            <Button onClick={handleLoginDialog}>
+              <h2 className="header-text">Login</h2>
+            </Button>
+            <LoginDialog
+              toSignUp={handleSignUpDialog}
+              open={loginDialog}
+              handleClose={handleLoginDialogClose}
+            />
+            <Button onClick={handleSignUpDialog}>
+              <h2 className="header-text">Sign Up</h2>
+            </Button>
+            <SignUpDialog
+              toLogin={handleLoginDialog}
+              open={signUpDialog}
+              handleClose={handleSignUpDialogClose}
+            />
+          </Box>
+          <Box sx={{ display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="show more"
+              aria-controls={mobileMenuId}
+              aria-haspopup="true"
+              onClick={handleMobileMenuOpen}
+              sx={{ color: "#222222" }}
+            >
+              <MoreIcon />
+            </IconButton>
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+      {renderMenu}
+    </Box>
+  );
 }
--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -4,14 +4,14 @@ import MovingText from "../../components/common/MovingText";
 import fw from "@assets/svg/FW200.svg";
 
 const HomePage = () => {
-	return (
-		<div className='App'>
-			<div className='image-container'>
-				<img src={fw} alt='fw' className='logo-pic' />
-			</div>
-			<MovingText />
-		</div>
-	);
+  return (
+    <div className="App">
+      <div className="image-container">
+        <img src={fw} alt="fw" className="logo-pic" />
+      </div>
+      <MovingText />
+    </div>
+  );
 };
 
 export default HomePage;
