7c8361f25bd88dc5e4f06b754f92c58758885295
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-04-22 12:10:27 +0300

    FAIR-56 faq draft

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,3 +1,4 @@
+import FaqSection from "@components/home/FaqSection";
 import BrandSection from "../../components/home/BrandSection";
 import HeroSection from "../../components/home/HeroSection";
 import "./HomePage.css";
@@ -7,6 +8,7 @@ const HomePage = () => {
 		<div className="home-page-container">
 			<HeroSection />
 			<BrandSection />
+			<FaqSection />
 		</div>
 	);
 };
