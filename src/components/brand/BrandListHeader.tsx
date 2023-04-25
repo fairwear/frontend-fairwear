827b8da95d3ba93980b413fb2e3a19a65c3eea43
Dominykas3000 <dominykas.svilpa@mif.stud.vu.lt>
2023-04-25 22:42:19 +0300

    FAIR-69 brand list page initial stage and creation

---
+++ src/components/brand/BrandListHeader.tsx
@@ -0,0 +1,15 @@
+import { Typography } from '@mui/material'
+import fw from "@assets/svg/FW200.svg";
+import './BrandComponents.css';
+
+export default function BrandListHeader() {
+    return (
+        <div>
+            <div >
+                <img src={fw} alt="fw" className="logo-pic" />
+            </div>
+            <Typography variant='h1'> Brand List</Typography>
+        </div>
+    )
+}
+
---
+++ src/pages/brand_list/BrandListPage.tsx
@@ -0,0 +1,14 @@
+import BrandList from "@components/brand/BrandList";
+import BrandListHeader from "@components/brand/BrandListHeader";
+import BrandListMovingText from "@components/common/BrandListMovingText";
+
+export default function BrandListPage() {
+  return (
+    <div>
+      <BrandListHeader />
+      <BrandListMovingText />
+      <BrandList />
+    </div>
+  )
+}
+
