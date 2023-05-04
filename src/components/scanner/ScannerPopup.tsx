ff88c35e29ef8f60f4eb79ec54ad0c8704c5deab
Survila Kernius <kernius.survila@mif.stud.vu.lt>
2023-05-04 18:40:44 +0300

    FAIR-73 build fox

--- src/components/scanner/ScannerPopup.tsx
+++ src/components/scanner/ScannerPopup.tsx
@@ -1,1 +1,9 @@
-const ScannerPopup = () => {};
+const ScannerPopup = () => {
+	return (
+		<div className="scanner-popup-container">
+			<div className="scanner-popup"></div>
+		</div>
+	);
+};
+
+export default ScannerPopup;
