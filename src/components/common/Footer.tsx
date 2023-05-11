f14b61c8c5dfe0778a61294e2ab1b9f3dd6f0579
Morta <morta.matelyte@mif.stud.vu.lt>
2023-05-11 15:49:00 +0300

    footer done

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,56 +1,78 @@
 import React from "react";
 import "./Footer.css";
+import fw from "@assets/svg/FW200.svg";
 import instagram from "../../assets/images/instagram_logo.png";
 
 const Footer = () => {
 	return (
 		<div className="footer">
-			<div className="sb__footer section__padding">
-				<div className="sb__footer-links">
-					<div className="sb__footer-links-div">
+			<div className="footer section__padding">
+				<div className="footer-links">
+					<div className="footer-links-div">
+						<div className="logo">
+							<img src={fw} alt="fw" className="logo-image" />
+						</div>
+					</div>
+					<div className="footer-links-div">
 						<a href="/contribute">
 							<p>Contribute</p>
 						</a>
-                        </div>
-                    <div className="sb__footer-links-div"> 
-                        <a href="/termsofservice">
-							<p>Terms Of Service</p>
+					</div>
+					<div className="footer-links-div">
+						<a href="/link2">
+							<p>Link2</p>
 						</a>
-                    </div>
-                    <div className="sb__footer-links-div">
-                        <a href="/report">
-							<p>Report</p>
+					</div>
+					<div className="footer-links-div">
+						<a href="/link3">
+							<p>Link3</p>
 						</a>
-                    </div>
-                    <div className="sb__footer-links-div">
-                        <a href="/surprise">
-							<p>Open</p>
+					</div>
+					<div className="footer-links-div">
+						<a href="/link4">
+							<p>Link4</p>
 						</a>
-                    </div>
-                    <div className="sb__footer-links-div">
-                        <h4>Coming soon on</h4>
-                        <div className="socialmedia">
-                            <p><img src={instagram} alt=""/></p>
-                        </div>
+					</div>
+					<div className="footer-links-div">
+						<a href="/link5">
+							<p>Link5</p>
+						</a>
+					</div>
+					<div className="footer-links-div">
+						<div className="socialmedia">
+							<p>
+								<img src={instagram} alt="" />
+							</p>
+						</div>
 					</div>
 				</div>
 
-         <hr></hr>
+				<hr></hr>
 
-         <div className="sb__footer-bellow">
-            <div className="sb__footer-bellow-copyright">
-                <p>
-                @{new Date().getFullYear()} FairWear. All right reserved.
-                </p>
-            </div>
-            <div className="sb__footer-bellow-links">
-                <a href="/idk"><div><p>I HAVE NO FUCKING IDEA WHAT IM DOING</p></div></a>
-                <a href="/help"><div><p>SEND HELP</p></div></a>
-            </div>
-
-         </div>
+				<div className="footer-bellow">
+					<div className="footer-bellow-copyright">
+						<p>@{new Date().getFullYear()} FairWear. All right reserved.</p>
+					</div>
+					<div className="footer-bellow-links">
+						<a href="/privacypolicy">
+							<div>
+								<p>Privacy Policy</p>
+							</div>
+						</a>
+						<a href="/termsofservice">
+							<div>
+								<p>Terms Of Service</p>
+							</div>
+						</a>
+						<a href="/cookiessettings">
+							<div>
+								<p>Cookies Settings</p>
+							</div>
+						</a>
+					</div>
+				</div>
 			</div>
-        </div>
+		</div>
 	);
 };
 
